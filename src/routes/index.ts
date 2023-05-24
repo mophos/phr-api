08b63688601f89757d2a1ee59c82e240a2a79b2a
Naphattharawat S <naphattharawat@gmail.com>
2023-05-24 12:44:19 +0700

    update

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,19 +9,15 @@ import { AlgorithmModel } from '../models/v1/algorithm';
 const algoritm = new AlgorithmModel();
 
 router.get('/', async(req: Request, res: Response) => {
-  console.log(req.body);
-  
-  // const rs0:any = await PersonalVisit.find({});
-  // const rs:any = await Users.find({});
-  // console.log(rs0[0]._doc);
-  // console.log(rs[0]._doc);
-  // const a = rs0[0]._doc;
-  // console.log(a);
-  
-  // console.log(rs);
-  
+  console.log(req.body); 
   res.send({ ok: true, message: 'Welcome to RESTful api server!', code: HttpStatus.OK });
 });
+
+router.post('/', async(req: Request, res: Response) => {
+  console.log(req.body); 
+  res.send({ ok: true, message: 'Welcome to RESTful api server!', code: HttpStatus.OK });
+});
+
 router.get('/version', async(req: Request, res: Response) => {
   // const rs0:any = await PersonalVisit.find({});
   // const rs:any = await Users.find({});
